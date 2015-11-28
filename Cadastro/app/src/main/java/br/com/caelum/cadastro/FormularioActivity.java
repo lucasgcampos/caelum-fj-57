f16c2b00218e1728529f2e8ea31426ffd1f970b9
Lucas Campos <lucasgcampos.contato@gmail.com>
2015-11-28 11:51:29 -0200

    Importe das imagens - Menu

--- Cadastro/app/src/main/java/br/com/caelum/cadastro/FormularioActivity.java
+++ Cadastro/app/src/main/java/br/com/caelum/cadastro/FormularioActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class FormularioActivity extends ActionBarActivity {
@@ -19,21 +22,18 @@ public class FormularioActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_formulario, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(this, "OK clicado", Toast.LENGTH_SHORT);
+                finish();
+                return false;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
